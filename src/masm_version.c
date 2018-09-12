80b8b19711e769cd4c2112a3546d186aa849e95b
Jim Eli <31859133+JimEli@users.noreply.github.com>
2018-09-12 16:04:10 -0700

    Update masm_version.c

--- src/masm_version.c
+++ src/masm_version.c
@@ -4,7 +4,8 @@
 * Author: James Eli
 * Date: 12/13/2017
 *
-* Demonstrates C & MASM Project Setup and a little bit of nonsense.
+* Demonstrates C string functions in x86 inline masm and gcc assembly.
+* Each function shows the basic C language equivalent.
 *
 * Notes:
 *  (1) Little to no error checking/input validation.
@@ -15,12 +16,10 @@
 *************************************************************************
 * Change Log:
 *   12/13/2017: Initial release. JME
+*   09/12/2018: Masm version added. JME
 *************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-
-// C/C++ Preprocessor Definitions: _CRT_SECURE_NO_WARNINGS
-#pragma warning(disable:4996) 
 
 char string[] = "abcdefghijklm"; // A string.
 char stringCopy[14] = { 0 };     // An empty string.
@@ -32,16 +31,16 @@ char stringCopy[14] = { 0 };     // An empty string.
 //}
 __inline unsigned int strLen(const char *s) {
 	__asm {
-		mov   edi, s			; Load string address.
+		mov   edi, s		; Load string address.
 		or    ecx, 0ffffffffh	; Init for counting down.
-		xor   eax, eax			; Zero.
-		cmp   edi, 0			; Check for null string.
+		xor   eax, eax		; Zero.
+		cmp   edi, 0		; Check for null string.
 		je    len1
-		repne scasb				; Repeat until zero.
+		repne scasb		; Repeat until zero.
 
-		not   ecx				; Invert
-		dec   ecx				; Subtract 1.
-		mov   eax, ecx			; Prepare return value.
+		not   ecx		; Invert
+		dec   ecx		; Subtract 1.
+		mov   eax, ecx		; Prepare return value.
 	len1:
 	}
 }
@@ -75,22 +74,22 @@ __inline char *strCpy(char *d, const char *s)
 //  while (*cp++ = *src++);
 //  return dst;
 //}
-__inline char *strCat(char * dest, const char * src)
+__inline char *strCat(char * d, const char * s)
 {
-	char *cp = dest;
+	char *cp = d;
 
 	__asm {
 		; while (*cp)
-		mov   ecx, src
+		mov   ecx, s
 		mov   eax, cp
 	cat1:
 		movsx ebx, byte ptr [eax]
 		test  ebx, ebx
 		je    cat2
-		;  cp++;
+		;  cp++
 		inc   eax
 		jmp   cat1
-		; while (*cp++ = *src++);
+		; while (*cp++ = *src++)
 	cat2:
 		mov   dl, byte ptr[ecx]
 		mov   byte ptr[eax], dl
@@ -99,9 +98,8 @@ __inline char *strCat(char * dest, const char * src)
 		cmp   dl, 0
 		je    cat3
 		jmp   cat2
-		; return dest;
 	cat3:
-		mov   eax, dest
+		mov   eax, d	; return d
 	}
 }
 
@@ -115,19 +113,19 @@ __inline char *strCat(char * dest, const char * src)
 __inline char *strChr(const char *s, int c) 
 {
 	__asm {
-		mov  edi, s				; edi == source ptr.
-		mov  ebx, c				; cl == target.
-		mov  al, 0				; Set to null byte.
-		xor  ecx, ecx			; Zero.
+		mov  edi, s		; edi == source ptr.
+		mov  ebx, c		; cl == target.
+		mov  al, 0		; Set to null byte.
+		xor  ecx, ecx		; Zero.
 	schr:
 		mov  cl, byte ptr[edi]	; Get a character.
-		cmp  bl, cl				; Check if character is target.
-		jz   schr_exit			; Jump to exit if match.
-		scasb					; Check if null byte.
-		jnz  schr				; Loop if no match.
-		mov  edi, 0				; Return null.
+		cmp  bl, cl		; Check if character is target.
+		jz   schr_exit		; Jump to exit if match.
+		scasb			; Check if null byte.
+		jnz  schr		; Loop if no match.
+		mov  edi, 0		; Return null.
 	schr_exit:
-		mov eax, edi			; Return results.
+		mov eax, edi		; Return results.
 	}
 }
 
@@ -144,21 +142,21 @@ __inline int strCmp(const char *s1, const char *s2)
 	__asm {
 		mov esi, s1
 		mov edi, s2
-		push edi		; determine length of string
+		push edi	; determine length of string
 		call strLen
-		add  esp, 4		; clean up stack
+		add  esp, 4	; clean up stack
 		mov  ebx, eax
-		push esi		; determine length of string
+		push esi	; determine length of string
 		call strLen
-		add  esp, 4		; clean up stack
+		add  esp, 4	; clean up stack
 		cmp  eax, ebx	; compare lengths
 		ja   greater	; first string is longer
-		jb   less		; second string is longer
+		jb   less	; second string is longer
 		mov  ecx, eax	; length of strings
-		repe cmpsb		; compare strings
+		repe cmpsb	; compare strings
 		jg   greater	; first string is greater
-		jl   less		; second string is greater
-		mov  r, 0		; strings are equal
+		jl   less	; second string is greater
+		mov  r, 0	; strings are equal
 		jmp  strcmp_exit
 		greater:
 		mov  r, 1
@@ -187,53 +185,109 @@ __inline int strCmp(const char *s1, const char *s2)
 //    }
 //    return NULL;
 //  }
-__inline char *strStr(const char *src, const char *target) 
+__inline char *strStr(const char *s, const char *target) 
 {
 	__asm {
-		mov  esi, src
+		mov  esi, s
 		mov  edi, target
-		push esi		; Save src addr.
-		push edi		; Pass target string to strlen.
+		push esi	; Save src addr.
+		push edi	; Pass target string to strlen.
 		call strLen
-		add  esp, 4		; Clean up stack.
+		add  esp, 4	; Clean up stack.
 		push eax
-
-		push esi		; Pass src string to strlen.
+			
+		push esi	; Pass src string to strlen.
 		call strLen
-		add  esp, 4		; Clean up stack.
+		add  esp, 4	; Clean up stack.
 
-		pop  ecx		; Length of src string.
+		pop  ecx	; Length of src string.
 		cmp  ecx, eax	; Compare target to src.
-		ja   str2		; Jump if target longer than src.
+		ja   str2	; Jump if target longer than src.
 
-		cld				; Direction up.
+		cld		; Direction up.
 		sub  eax, ecx	; Subtract target from src length.
 		mov  ebx, eax	; Loop counter.
 		mov  eax, ecx	; eax == target length.
 		xor  eax, eax
 
 	str1:
-		inc  eax		; Index counter.
-		inc  esi		; Advance to next src char.
+		inc  eax	; Index counter.
+		inc  esi	; Advance to next src char.
 		push esi
 		push edi
 		push ecx
-		rep  cmpsb		; Search for target in src at this index.
+		rep  cmpsb	; Search for target in src at this index.
 		pop  ecx
 		pop  edi
 		pop  esi
 
-		je   str3		; found target
-		dec  ebx		; decrement loop counter
-		jnz  str1		; repeat search
+		je   str3	; found target
+		dec  ebx	; decrement loop counter
+		jnz  str1	; repeat search
 	
 	str2:
 		xor  eax, eax	; not found, zeroize eax
 
 	str3:
-		pop ecx			; pop start of src string
+		pop ecx		; pop start of src string
 		add  ecx, eax	; add index of target if found
 		mov  eax, ecx	; save return pointer
+	}
+}
+
+// The 'strncpy' function copies not more than `n' characters (characters
+// that follow a null character are not copied) from the array pointed to
+// by 'src' to the array pointed to by 'dest'.  If copying takes place between
+// objects that overlap, the behavior is undefined.
+// If the array pointed to by 'src' is a string that is shorter than 'n'
+// characters, null characters are appended to the copy in the array
+// pointed to by 'dest', until 'n' characters in all have been written.
+// The 'strncpy' function returns the value of 'dest'.
+//char* strncpy_(char *dest, const char *src, size_t n) {
+//	char *s = dest;
+//	while (n > 0 && *src != '\0') {
+//		*s++ = *src++;
+//		--n;
+//	}
+//	while (n > 0) {
+//		*s++ = '\0';
+//		--n;
+//	}
+//	return dest;
+//}
+__inline char* strnCpy(char *d, const char *s, size_t n) {
+	char *cd = d;
+
+	__asm {
+		mov   eax, s
+		mov   ebx, cd
+		mov   ecx, n
+		; while (n > 0 && *src != '\0') 
+	snc1:
+		cmp   ecx, 0
+		jbe   snc2
+		movzx edx, byte ptr[eax]
+		test  edx, edx
+		je    snc2
+		;	 *s++ = *src++;
+		mov   byte ptr[ebx], dl
+		add   ebx, 1
+		add   eax, 1
+		;	--n;
+		sub   ecx, 1
+		jmp   snc1
+		; while (n > 0) 
+	snc2:
+		cmp   ecx, 0
+		jbe   snc_exit
+		;	*s++ = '\0';
+		mov   byte ptr[ebx], 0
+		add   ebx, 1
+		;	--n;
+		sub   ecx, 1
+		jmp   snc2
+	snc_exit:
+		mov   eax, d
 	}
 }
 
@@ -254,7 +308,7 @@ __inline char *strStr(const char *src, const char *target)
 		mov esi, s
 		mov edi, d
 		mov ecx, n
-		cld			; direction = up
+		cld		; direction = up
 		rep movsb	; Copy string byte from esi to edi until ecx == 0
 	}
 
@@ -279,14 +333,80 @@ __inline void *memSet(void *d, int c, size_t n)
 	}
 }
 
- // Reverse string using the stack.
- char *reverse(const char *src) 
- {
-	 char *dest = (char *)src;
+// Insert sub-string s into string d at location n.
+__inline void strIns(char *d, const char *s, const size_t n) {
+	size_t i = 0;
+	char *temp = NULL;
 
 	__asm {
-		 xor ecx, ecx
-		 mov esi, src
+		; temp = (char*)malloc(strlen(d) + strlen(s) + 1);
+		push  n		; Preserve n.
+		push  d		; Get length of d.
+		call  strLen
+		add   esp, 4
+		mov   i, eax	; i = strlen(d)
+
+		push  s		; Get length of s.
+		call  strLen
+		add   esp, 4
+		add   i, eax	; i = strlen(d) + strlen(s)
+		inc   i		; +1 for terminating null.
+
+		push  i		; Reserve memory.
+		call  malloc
+		add   esp, 4
+		cmp   eax, 0	; Malloc fail?
+		je    strins_exit
+		mov   temp, eax	; Save addr in temp.
+
+		; strcpy_(temp, d);
+		push  d		; Copy d to temp.
+		push  temp
+		call  strCpy
+		add   esp, 8
+
+		; temp[n] = '\0';
+		pop   n		; Restore n.
+		mov   eax, temp	; eax = temp.
+		add   eax, n	; eax = temp + n
+		mov byte ptr[eax], 0	; Null terminate string.
+
+		; strcat_(temp, s);
+		push  s		; Concatenate s to temp. 
+		push  temp
+		call  strCat
+		add   esp, 8
+
+		; strcat_(temp, d + n);
+		mov   eax, d	; eax = d
+		add   eax, n	; eax = d + n
+		push  eax
+		push  temp
+		call  strCat	; Concatenate d+n to temp.
+		add   esp, 8
+
+		; strcpy_(d, temp);
+		push  temp	; Copy temp to d.
+		push  d
+		call  strCpy
+		add   esp, 8
+
+		; free(temp);
+		push  temp	; Free temp memory.
+		call  free
+		add   esp, 4
+	strins_exit:
+	}
+}
+
+// Reverse string using the stack.
+ char *reverse(const char *s) 
+ {
+	char *cs = (har *)s;
+
+	__asm {
+		xor ecx, ecx	
+		mov esi, s
 	 L1:
 		movzx edx, byte ptr[esi + ecx]
 		cmp dl, 0
@@ -305,7 +425,7 @@ __inline void *memSet(void *d, int c, size_t n)
 	L4:
 	}
 
-	return dest;
+	return cs;
 }
 
 void printStrings(const char *pc, const char *p) {
@@ -345,157 +465,14 @@ int main(void) {
 	printStrings(stringCopy, string);
 
 	// strstr
-	const char haystack[20] = "ThisAintNoFreeLunch";
+	char haystack[20] = "ThisAintNoFreeLunch";
 	const char needle[10] = "Free";
 	printf("substring from strstr = %s\n", strStr(haystack, needle));
+
+	// strnCpy
+	puts("strncpy:"); strnCpy(haystack, needle, 5); printStrings(needle, haystack);
+
+	// strIns
+	char abc[12] = "123456";
+	puts("strIns:"); strIns(abc, needle, 3); printStrings(abc, needle);
 }
-
-/*
-// The 'strncpy' function copies not more than `n' characters (characters
-// that follow a null character are not copied) from the array pointed to
-// by 'src' to the array pointed to by 'dest'.  If copying takes place between
-// objects that overlap, the behavior is undefined.
-// If the array pointed to by 'src' is a string that is shorter than 'n'
-// characters, null characters are appended to the copy in the array
-// pointed to by 'dest', until 'n' characters in all have been written.
-// The 'strncpy' function returns the value of 'dest'.
-char* strncpy_(char *dest, const char *src, size_t n)
-{
-	char *s = dest;
-
-	while (n > 0 && *src != '\0')
-	{
-		*s++ = *src++;
-		--n;
-	}
-	while (n > 0)
-	{
-		*s++ = '\0';
-		--n;
-	}
-
-	return dest;
-}
-
-strncpy_:
-	push    %ebp
-	mov     %esp, %ebp
-	sub     $0x10, %esp
-	//char *s = dest;
-	mov     0x8(%ebp), %eax
-	mov     %eax, -0x4(%ebp)
-	//while (n > 0 && *src != '\0')
-	jmp     strncpy_ + 41
-	// *s++ = *src++;
-	mov     -0x4(%ebp), %eax
-	lea     0x1(%eax), %edx
-	mov     %edx, -0x4(%ebp)
-	mov     0xc(%ebp), %edx
-	lea     0x1(%edx), %ecx
-	mov     %ecx, 0xc(%ebp)
-	movzbl  (%edx), %edx
-	mov     %dl, (%eax)
-	// --n;
-	subl    $0x1, 0x10(%ebp)
-
-	cmpl    $0x0, 0x10(%ebp)
-	je      strncpy_ + 75
-	mov     0xc(%ebp), %eax
-	movzbl  (%eax), %eax
-	test    %al, %al
-	jne     strncpy_ + 14
-	//while (n > 0)
-	jmp     strncpy_ + 75
-	// *s++ = '\0';
-	mov     -0x4(%ebp), %eax
-	lea     0x1(%eax), %edx
-	mov     %edx, -0x4(%ebp)
-	movb    $0x0, (%eax)
-	// --n;
-	subl    $0x1, 0x10(%ebp)
-
-	cmpl    $0x0, 0x10(%ebp)
-	jne     strncpy_ + 59
-	//return dest;
-	mov     0x8(%ebp), %eax
-	leave
-
-
-//void strins(char* d, char* s, int n) {
-//  char *temp;
-//  temp = (char*)malloc(strlen_(d) + strlen_(s) + 1);
-	//strncpy(temp, d, n);
-//  strcpy_(temp, d);
-//  temp[n] = '\0';
-//  strcat_(temp, s);
-//  strcat_(temp, d + n);
-//  strcpy_(d, temp);
-//  free(temp);
-//}
-void strins_(char *d, const char *s, const size_t n) {
-	size_t i = 0;
-	char *temp = NULL;
-
-	asm volatile (
-		//temp = (char*)malloc(strlen(d) + strlen(s) + 1);
-		"push  %3            \n" // preserve n
-		"push  %0            \n" // d
-		"call  _strlen_      \n"
-		"add   $4, %%esp     \n"
-		"mov   %%eax, %4     \n" // i = strlen(d)
-
-		"push  %1            \n" // s
-		"call  _strlen_      \n"
-		"add   $4, %%esp     \n"
-		"add   %%eax, %4     \n" // i = strlen(d) + strlen(s)
-		"inc   %4            \n" // +1 for terminating null
-
-		"push  %4            \n" // reserve memory
-		"call  _malloc       \n"
-		"add   $4, %%esp     \n"
-		"cmp   $0, %%eax     \n" // malloc fail?
-		"je    strins_exit   \n"
-		"mov   %%eax, %2     \n" // temp
-
-		//strcpy_(temp, d);
-		"push  %0            \n" // d
-		"push  %2            \n" // temp
-		"call  _strcpy_      \n"
-		"add   $8, %%esp     \n"
-
-		//temp[n] = '\0';
-		"pop %3              \n" // preserve n
-		"mov   %2, %%eax     \n" // eax = temp
-		"add   %3, %%eax     \n" // eax = temp + n
-		"movb  $0x0, (%%eax) \n" // null terminate string
-
-		//strcat_(temp, s);
-		"push  %1            \n" // s
-		"push  %2            \n" // temp
-		"call  _strcat_      \n"
-		"add   $8, %%esp     \n"
-
-		//strcat_(temp, d + n);
-		"mov   %0, %%eax     \n" // eax = d
-		"add   %3, %%eax     \n" // eax = d + n
-		"push  %%eax         \n"
-		"push  %2            \n" // temp
-		"call  _strcat_      \n"
-		"add   $8, %%esp     \n"
-
-		//strcpy_(d, temp);
-		"push  %2            \n" // temp
-		"push  %0            \n" // d
-		"call  _strcpy_      \n"
-		"add   $8, %%esp     \n"
-
-		//free(temp);
-		"push  %2            \n" // temp
-		"call   _free        \n"
-		"add   $4, %%esp     \n"
-		"strins_exit:        \n"
-
-		: : "D" (d), "S" (s), "b" (temp), "d" (n), "c" (i) : "eax", "memory"
-		);
-}
-*/
